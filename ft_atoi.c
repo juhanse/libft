61a2bc2097a1022958a2cf884be907fd8a9d471e
julienhanse <shibaz.contact@gmail.com>
2024-10-04 20:28:43 +0200

    fix: ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,10 +6,22 @@ int	ft_atoi(char *str)
 	int	sign;
 	int	result;
 
+<<<<<<< HEAD
 	i = 1;
 	sign = 1;
 	if (str[0] == '-')
 		sign = -1;
+=======
+	i = 0;
+	sign = 1;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+>>>>>>> e2b6327 (add: ft_atoi.c)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 - (str[i] + 48);
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int		ft_strlen(char *str);
@@ -14,4 +15,4 @@ int		ft_strcmp(char *s1, char *s2);
 int		ft_strncmp(char *s1, char *s2, int size);
 int		ft_atoi(char *str);
 
-#endif MAIN_H
+#endif
