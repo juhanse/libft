733bc7609fb13bdf6948ba7f86e0c860a6b8fce7
julienhanse <shibaz.contact@gmail.com>
2024-10-04 20:36:59 +0200

    fix: ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,6 @@ int	ft_atoi(char *str)
 	int	sign;
 	int	result;
 
-<<<<<<< HEAD
-	i = 1;
-	sign = 1;
-	if (str[0] == '-')
-		sign = -1;
-=======
 	i = 0;
 	sign = 1;
 	if (str[i] == '-')
@@ -21,7 +15,6 @@ int	ft_atoi(char *str)
 	}
 	else if (str[i] == '+')
 		i++;
->>>>>>> e2b6327 (add: ft_atoi.c)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 - (str[i] + 48);
