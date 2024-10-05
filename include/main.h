5d7b2d5c9ba65cba5f0cef5d1e244c78b9011990
julienhanse <shibaz.contact@gmail.com>
2024-10-05 12:17:22 +0200

    add: ft_isascii.c

--- include/main.h
+++ include/main.h
@@ -16,5 +16,7 @@ int		ft_strncmp(char *s1, char *s2, int size);
 int		ft_atoi(char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 #endif
