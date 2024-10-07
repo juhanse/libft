4839c317bdace34ab32eef3c6505d57d03fd244c
julienhanse <shibaz.contact@gmail.com>
2024-10-07 22:12:20 +0200

    add: add folder

---
+++ add/strrchr.c
@@ -0,0 +1,15 @@
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = ft_strlen(s);
+	while (i >= 0)
+	{
+		if (s[i] == (char) c)
+			return ((char *) s + i);
+		i--;
+	}
+	return (NULL);
+}
