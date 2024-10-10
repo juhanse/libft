a1b4c03dfb5dda888d78ad0d12ec463014e09ab0
julienhanse <shibaz.contact@gmail.com>
2024-10-10 22:24:26 +0200

    add: ft_memcpy

---
+++ srcs/ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julienhanse <julienhanse@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 22:24:04 by julienhanse       #+#    #+#             */
+/*   Updated: 2024/10/10 22:24:12 by julienhanse      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+void		*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
