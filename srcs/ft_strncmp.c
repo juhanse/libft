bc0a7faf70015ff768c7284cc335d1697b8c2880
julienhanse <shibaz.contact@gmail.com>
2024-10-10 22:10:49 +0200

    fix: while on strcmp.c

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -5,7 +5,7 @@ int	ft_strncmp(char *s1, char *s2, int size)
 	int		i;
 
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < size)
+	while (s1[i] && s1[i] == s2[i] && i < size)
 		i++;
 	return (s1[i] - s2[i]);
 }
