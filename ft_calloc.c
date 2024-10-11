584fb9475cbec1969d7c2592adf015240cc72df3
julienhanse <shibaz.contact@gmail.com>
2024-10-11 18:36:45 +0200

    fix: norme of ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,16 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*   By: julienhanse <julienhanse@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:47:31 by juhanse           #+#    #+#             */
-/*   Updated: 2024/10/11 15:55:30 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/10/11 18:36:23 by julienhanse      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_calloc(size_t num, size_t size)
 {
-	size_t	len;
+	size_t	total_size;
+	size_t	i;
+	void	*ptr;
 
-	len = num * size;
+	i = 0;
+	total_size = num * size;
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	while (i < total_size)
+	{
+		((unsigned char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
 }
