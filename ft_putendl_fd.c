dc78b544a045d99bbadcd62ec167986a368dd2ab
julienhanse <shibaz.contact@gmail.com>
2024-10-30 00:23:25 +0100

    fix: use putchar_fd function in ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: julienhanse <julienhanse@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:31:33 by julienhanse       #+#    #+#             */
-/*   Updated: 2024/10/30 00:18:33 by julienhanse      ###   ########.fr       */
+/*   Updated: 2024/10/30 00:22:58 by julienhanse      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	ft_putendl_fd(char *s, int fd)
 	if (!s)
 		return ;
 	ft_putstr_fd(s, fd);
-	write(fd, "\n", 1);
+	ft_putchar_fd('\n', fd);
 }
