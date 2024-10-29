50b04cec8ae3f3fa9a06643a8b17d381b7a0c55e
julienhanse <shibaz.contact@gmail.com>
2024-10-30 00:30:41 +0100

    fix: retun in while ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*   By: julienhanse <julienhanse@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 22:22:20 by julienhanse       #+#    #+#             */
-/*   Updated: 2024/10/26 18:12:21 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/10/30 00:30:13 by julienhanse      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size)
 	i = 0;
 	if (size == 0)
 		return (0);
-	while (s1[i] && s1[i] == s2[i] && i < size - 1)
+	while ((s1[i] || s2[i]) && i < size)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
